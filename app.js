6923ea194c71391f10598abb3a9758073c6c3b78
Deepak singh <manishsingh111095@gmail.com>
2023-08-21 10:24:54 +0530

    changes10-21-23

--- app.js
+++ app.js
@@ -7,27 +7,67 @@ import {
 initTE({ Carousel });
 
 //Product slider
-let defaultTransform = 0;
+// let defaultTransform = 0;
+// const slideWidth = 398; // Width of each slide item
+// const itemsPerSlide = 4; // Number of items per slide
+
+// function goNext() {
+//     defaultTransform = defaultTransform - (slideWidth * itemsPerSlide);
+//     var slider = document.getElementById("slider");
+//     if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+//     slider.style.transform = "translateX(" + defaultTransform + "px)";
+// }
+
+// next.addEventListener("click", goNext);
+
+// function goPrev() {
+//     var slider = document.getElementById("slider");
+//     if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+//     else defaultTransform = defaultTransform + (slideWidth * itemsPerSlide);
+//     slider.style.transform = "translateX(" + defaultTransform + "px)";
+// }
+
+// prev.addEventListener("click", goPrev);
+
+
 const slideWidth = 398; // Width of each slide item
-const itemsPerSlide = 4; // Number of items per slide
+let itemsPerSlide = 4; // Number of items per slide (default)
+
+function updateItemsPerSlide() {
+  // Check the viewport width and adjust itemsPerSlide as needed
+  if (window.innerWidth <= 768) {
+    console.log("Script is running");
+    itemsPerSlide = 1; // Show only one item in mobile view (adjust the breakpoint as needed)
+  } else {
+    console.log("Script is not running");
+    itemsPerSlide = 4; // Show four items in larger screens
+  }
+}
+
+// Call the updateItemsPerSlide function initially and whenever the window is resized
+updateItemsPerSlide();
+window.addEventListener('resize', updateItemsPerSlide);
+
+let defaultTransform = 0;
 
 function goNext() {
-    defaultTransform = defaultTransform - (slideWidth * itemsPerSlide);
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
+  defaultTransform = defaultTransform - (slideWidth * itemsPerSlide);
+  var slider = document.getElementById("slider");
+  if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+  slider.style.transform = "translateX(" + defaultTransform + "px)";
 }
 
 next.addEventListener("click", goNext);
 
 function goPrev() {
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + (slideWidth * itemsPerSlide);
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
+  var slider = document.getElementById("slider");
+  if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+  else defaultTransform = defaultTransform + (slideWidth * itemsPerSlide);
+  slider.style.transform = "translateX(" + defaultTransform + "px)";
 }
 
 prev.addEventListener("click", goPrev);
+
 
 
 
